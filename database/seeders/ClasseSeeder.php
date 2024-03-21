b5b374c082f53557cd5603616278706b3f44b38c
will henrique <willhc1984@gmail.com>
2024-03-21 16:44:16 -0300

    Relação 1:N Cursos/Aulas - Seeder de Aulas

---
+++ database/seeders/ClasseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Classe;
+use Illuminate\Database\Seeder;
+
+class ClasseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(!Classe::where('name', 'Aula 1'){
+            Classe::create([
+                'name'=> 'Aula 1',
+                'descricao'=> 'Introdução',
+                'course_id'=> 5
+            ])
+        });
+
+        if(!Classe::where('name', 'Aula 1'){
+            Classe::create([
+                'name'=> 'Aula 1',
+                'descricao'=> 'Introdução',
+                'course_id'=> 4
+            ])
+        });
+
+        if(!Classe::where('name', 'Aula 1'){
+            Classe::create([
+                'name'=> 'Aula 1',
+                'descricao'=> 'Introdução',
+                'course_id'=> 2
+            ])
+        });
+    }
+}
