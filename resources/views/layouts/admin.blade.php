23c61a0b9f1ba5e4ab4255484667ab877cb179e1
will henrique <willhc1984@gmail.com>
2024-04-03 15:00:35 -0300

    Adicionando Bootstrap 5.1.3

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Celke</title>
+
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <title>Curso Celke</title>
 </head>
 
 <body>
 
     @yield('content')
     
-
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    
 </body>
 </html>
