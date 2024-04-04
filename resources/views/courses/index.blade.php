48db1f7790820f60e7143dc1d4b80f24e3038e03
will henrique <willhc1984@gmail.com>
2024-04-04 16:24:48 -0300

    Implementação Bootstrap - Dashboard Cursos

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -2,23 +2,83 @@
 
 @section('content')
 
-    <h1>Listar cursos</h1>
+    <div class="container-fluid px-4">
+        <div class="mb1 space-between-elements">
+            <h2 class="mt-3">Dashboard</h2>
+            <ol class="breadcrumb mb-3 mt-3">
+                <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                <li class="breadcrumb-item active">Cursos</li>
+            </ol>            
+        </div>
 
-        @if(session('success'))
-            <p style="color: #082">
-                {{ session('success') }}
-            </p>
-        @endif
+        <div class="card mb-4">
+            <div class="card-header space-between-elements">
+                <span>Listar</span>
+                <span>
+                    <a href="{{ route('course.create') }}" class="btn btn-success btn-sm">
+                        Cadastrar</a>
+                </span>
+            </div>
 
-        @if(session('error'))
-            <p style="color: #f00">
-                {{ session('error') }}
-            </p>
-        @endif
-        
-        <a href="{{ route('course.create') }}">Cadastrar curso</a><br>
+            <div class="card-body">
+                @if(session('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('success') }}
+                    </div>
+                @endif
 
-        <h1>Cursos cadastrados:</h1>
+                @if(session('error'))
+                    <div class="alert alert-danger" role="alert">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Preço</th>
+                            <th scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse($courses as $course)
+                            <tr>
+                                <th scope="row">{{ $course->id }}</th>
+                                <td>{{ $course->name }}</td>
+                                <td>{{ $course->price }}</td>
+                                <td>
+                                <span>
+                                    <a href="{{ route('course.create', ['course' => $course->id]) }}" class="btn btn-secondary btn-sm">
+                                        Visualizar</a>
+                                    <a href="{{ route('course.edit', ['course' => $course->id]) }}" class="btn btn-primary btn-sm">
+                                        Editar</a>
+                                        <form method="POST" action="{{ route('course.destroy', ['course' => $course->id]) }}">
+                                            @csrf
+                                            @method('delete')
+                                            <button class="btn btn-danger btn-sm" type="submit" onclick="return confirm('Tem certeza que desja excluir este registro?')">Apagar</button>
+                                        </form>
+                                </span>
+                                </td>
+                            </tr>
+                        @empty
+                            <div class="alert alert-danger" role="alert">
+                                Nenhum curso encontrado!
+                            </div>
+                        @endforelse
+                    </tbody>
+                </table>
+
+                {{ $courses->links() }}
+
+            </div>
+        </div>
+
+    </div>
+
+
+
         @forelse($courses as $course)
             ID: {{ $course->id }} <br>
             Nome: {{ $course->name }} <br>
@@ -41,6 +101,6 @@
             <p style="color: red;">Nenhum curso encontrado!</p>
         @endforelse
 
-        {{ $courses->links() }}
+        
 
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,7 @@
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styles_sbadmin.css') }}" rel="stylesheet">
     <link href="{{ asset('css/all.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 
     <title>Curso Celke</title>
 </head>
