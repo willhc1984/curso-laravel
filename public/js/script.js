f92f54c9d092d90ff3f291a35b2bd31212cc05dc
will henrique <willhc1984@gmail.com>
2024-07-01 10:52:29 -0300

    SweetAlert - error/success - varias mensagens no modal

--- public/js/script.js
+++ public/js/script.js
@@ -9,7 +9,7 @@ inputPrice.addEventListener('input', function(){
     //Obter o valor atual removendo qualquer caracter
     let valuePrice = this.value.replace(/[^\d]/g, '');
 
-    //Adcionar os separadores de milhares
+    //Adicionar os separadores de milhares
     var formatedPrice = (valuePrice.slice(0, -2).replace(/\B(?=(\d{3})+(?!\d))/g, '.')) + '' + valuePrice.slice(-2);
 
     //Adicionar virgula a ate dois digitos se houver centavos
--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,39 +1,55 @@
-@if(session('success'))
-{{-- <div class="alert alert-success" role="alert">
+@if (session('success'))
+    {{-- <div class="alert alert-success" role="alert">
     {{ session('success') }}
 </div> --}}
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        Swal.fire({
-            title: 'Pronto!',
-            html: '{{ session('success') }}',
-            icon: 'success'
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            Swal.fire({
+                title: 'Pronto!',
+                html: '{{ session('success') }}',
+                icon: 'success'
+            });
         });
-    });
-</script>
+    </script>
 @endif
 
-@if(session('error'))
-{{-- <div class="alert alert-danger" role="alert">
+@if (session('error'))
+    {{-- <div class="alert alert-danger" role="alert">
     {{ session('error') }}
 </div> --}}
-<script>
-    document.addEventListener('DOMContentLoaded', () => {
-        Swal.fire({
-            title: 'Erro:',
-            html: '{{ session('error') }}',
-            icon: 'error'
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            Swal.fire({
+                title: 'Erro:',
+                html: '{{ session('error') }}',
+                icon: 'error'
+            });
         });
-    });
-</script>
+    </script>
 @endif
 
 @if ($errors->any())
-<div class="alert alert-danger">
+    @php
+        $message = '';
+        foreach ($errors->all() as $error) {
+            $message .= $error . '<br>';
+        }
+    @endphp
+
+    <script>
+        document.addEventListener('DOMContentLoaded', () => {
+            Swal.fire({
+                title: 'Erro:',
+                html: '{!! $message !!}',
+                icon: 'error'
+            });
+        });
+    </script>
+{{-- <div class="alert alert-danger">
     <ul>
         @foreach ($errors->all() as $error)
             <li>{{ $error }}</li>
         @endforeach
     </ul>
-</div>
-@endif  
+</div> --}}
+@endif
